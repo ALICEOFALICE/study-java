22d7e8e106025ba4ed5104a909c67f36ccc2890c
桜火 <xie@loli.fit>
2021-03-03 17:29:39 +0800

    关系运算符

---
+++ src/RelationalOperator.java
@@ -0,0 +1,15 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+        System.out.println(1 >= 2);//计算1是否大于等于2
+        System.out.println(2 >= 2);//计算2是否大于等于2
+        System.out.println(3 >= 2);//计算3是否大于等于2
+        //=========================================//
+        System.out.println(1 <= 2);//计算1是否小于等于2
+        System.out.println(2 <= 2);//计算2是否小于等于2
+        System.out.println(3 <= 2);//计算3是否小于等于2
+        //=========================================//
+        System.out.println(2 == 2);//计算2是否等于2
+        System.out.println(2 != 2);//计算2是否不等于2
+
+    }
+}
